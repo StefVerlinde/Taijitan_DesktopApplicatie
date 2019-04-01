00780cc19931d513deaa4d227cdc0aea006694b7
Jarne Deschacht <jarne.deschacht@student.hogent.be>
2019-04-01 14:25:57 +0200

    Update login screen in JavaFX

---
+++ Taijitan/src/gui/LoginController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.GridPane;
+
+public class LoginController extends GridPane
+{
+    @FXML
+    private JFXPasswordField txtEmail;
+    @FXML
+    private JFXTextField txtPassword;
+    @FXML
+    private JFXButton btnLogin;
+
+    public LoginController(){
+        FXMLLoader loader = new FXMLLoader(getClass().
+                getResource("Login.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    @FXML
+    private void login(ActionEvent event)
+    {
+    }
+    
+}
--- Taijitan/src/taijitan/Taijitan.java
+++ Taijitan/src/taijitan/Taijitan.java
@@ -5,13 +5,16 @@
  */
 package taijitan;
 
+import gui.LoginController;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
@@ -21,36 +24,20 @@ public class Taijitan extends Application
 {
     
     @Override
-    public void start(Stage primaryStage)
-    {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>()
-        {
-            
-            @Override
-            public void handle(ActionEvent event)
-            {
-                System.out.println("Hello World!");
-            }
+    public void start(Stage stage) {
+        Scene scene = new Scene(new LoginController());
+        stage.setScene(scene);
+
+        // The stage will not get smaller than its preferred (initial) size.
+        stage.setOnShown((WindowEvent t) -> {
+            stage.setMinWidth(stage.getWidth());
+            stage.setMinHeight(stage.getHeight());
         });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
-    {
-        launch(args);
+    public static void main(String... args) {
+        Application.launch(Taijitan.class, args);
     }
     
 }

91bff2142448fcb597e05349ab0dfdf6edf90064
Stef Verlinde <stef.verlinde@student.hogent.be>
2019-05-03 15:44:30 +0200

    Scorebord logic with table, pdf button and excel button

--- Taijitan/src/gui/ScorebordController.java
+++ Taijitan/src/gui/ScorebordController.java
@@ -3,6 +3,7 @@ package gui;
 import com.jfoenix.controls.*;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import domain.Domaincontroller;
+import domain.Rank;
 import domain.User;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -13,11 +14,26 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Callback;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ScorebordController extends AnchorPane {
     @FXML
@@ -74,10 +90,12 @@ public class ScorebordController extends AnchorPane {
         final TreeItem<User> root = new RecursiveTreeItem<User>(users, RecursiveTreeObject::getChildren);
         System.out.println(users.size());
 
-        tblScorebord = new JFXTreeTableView<>();
         tblScorebord.getColumns().setAll(clmUserFamilyName, clmUserFirstName,clmScore);
         tblScorebord.setRoot(root);
         tblScorebord.setShowRoot(false);
+        clmScore.setSortType(TreeTableColumn.SortType.DESCENDING);
+        tblScorebord.getSortOrder().add(clmScore);
+        tblScorebord.sort();
 
         searchFields();
     }
@@ -125,10 +143,150 @@ public class ScorebordController extends AnchorPane {
     }
 
     private  void printRegistredUsersPdf(){
+        try {
+            PDDocument document = new PDDocument();
+            PDPage pageOne = new PDPage();
+            PDPageContentStream contentStream = new PDPageContentStream(document, pageOne);
+            contentStream.beginText();
+            contentStream.setFont( PDType1Font.COURIER, 20 );
+            contentStream.setLeading(14.5f);
+            contentStream.newLineAtOffset(25, 750);
+            String text;
+            text = "Scorebord";
+            contentStream.showText(text);
+            contentStream.endText();
 
+            contentStream.beginText();
+            contentStream.setFont( PDType1Font.COURIER, 12 );
+            contentStream.setLeading(14.5f);
+            contentStream.newLineAtOffset(25, 700);
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Date date = new Date();
+            text = formatter.format(date);
+            contentStream.showText(text);
+            contentStream.endText();
+
+
+            int counter = 0;
+            List<User> gesorteerdeUsers = dc.getAllUsers();
+            Collections.sort(gesorteerdeUsers, Comparator.comparing(User::getScore).reversed());
+            for(User u : gesorteerdeUsers){
+                counter+=1;
+                contentStream.beginText();
+                contentStream.setFont( PDType1Font.COURIER, 9 );
+                contentStream.setLeading(14.5f);
+                contentStream.newLineAtOffset(25, 600-(counter*10));
+
+                text = String.format("%-20s %-20s %-20s %-20s %-20s",  u.getFirstName(),"|", u.getName(),"|", u.getScore());
+                contentStream.showText(text);
+                contentStream.endText();
+            }
+            System.out.println("content added");
+            contentStream.close();
+            document.addPage(pageOne);
+            String path = askPath(".pdf");
+            document.save(path);
+//            document.save("D:/my_doc.pdf");
+            document.close();
+        } catch (IOException e) {
+            System.out.println("locatie niet gevonden");
+            e.printStackTrace();
+        }
     }
 
     private void printRegistredUsersExcell() {
+        try {
+            String[] columns = {"Voornaam", "Familienaam", "Score"};
+            //https://www.callicoder.com/java-write-excel-file-apache-poi/
+            Workbook workbook = new XSSFWorkbook();
 
+            CreationHelper createHelper = workbook.getCreationHelper();
+
+            Sheet sheet = workbook.createSheet("Scorebord");
+
+            Font headerFont = workbook.createFont();
+            headerFont.setBold(true);
+            headerFont.setFontHeightInPoints((short) 14);
+            headerFont.setColor(IndexedColors.RED.getIndex());
+
+            CellStyle headerCellStyle = workbook.createCellStyle();
+            headerCellStyle.setFont(headerFont);
+
+            Row headerRow = sheet.createRow(0);
+
+            for (int i = 0; i < columns.length; i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(columns[i]);
+                cell.setCellStyle(headerCellStyle);
+            }
+
+            CellStyle dateCellStyle = workbook.createCellStyle();
+            dateCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd-MM-yyyy"));
+
+
+            int rownum = 1;
+            List<User> gesorteerdeUsers = dc.getAllUsers();
+            Collections.sort(gesorteerdeUsers, Comparator.comparing(User::getScore).reversed());
+            for (User u : gesorteerdeUsers) {
+                rownum++;
+                Row row = sheet.createRow(rownum);
+
+                row.createCell(0).setCellValue(u.getFirstName());
+                row.createCell(1).setCellValue(u.getName());
+                row.createCell(2).setCellValue(u.getScore());
+            }
+
+            for (int i = 0; i < columns.length; i++) {
+                sheet.autoSizeColumn(i);
+            }
+            FileOutputStream fileOut = new FileOutputStream(askPath(".xlsx"));
+            workbook.write(fileOut);
+            fileOut.close();
+
+
+            workbook.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found");
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            System.out.println("IO Exception");
+            e.printStackTrace();
+        }
+    }
+
+    private String askPath(String extension){
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        chooser.showOpenDialog(null);
+        File f = chooser.getSelectedFile();
+        String path = f.getAbsolutePath();
+
+        LocalDate now = LocalDate.now();
+        int Year = now.getYear();
+        int Month = now.getMonthValue();
+        int day = now.getDayOfMonth();
+
+        String fileName = String.format("%s%s%d%d%d%s", path, "/Scorebord", Year, Month, day, extension);
+        System.out.println(fileName);
+
+        return fileName;
+    }
+
+    @FXML
+    private void selectUserInList(MouseEvent event) {
+        if(event.getClickCount() == 2)
+        {
+            TreeItem<User> selectedUser = tblScorebord.getSelectionModel().getSelectedItem();
+            List<User> users = dc.getAllUsers();
+            if(users.contains(selectedUser.getValue())){
+                System.out.println(selectedUser);
+                fc.changeToMembersWithSelectedUser(selectedUser.getValue());
+            }
+            else {
+                AlertBoxController.BasicAlert("Error", selectedUser.getValue().getFirstName() + " " + selectedUser.getValue().getName() + " is geen bestaand lid meer.");
+            }
+        }
     }
 }

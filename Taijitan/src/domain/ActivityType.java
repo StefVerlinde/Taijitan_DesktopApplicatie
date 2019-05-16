8b46449333b60cc74c0c95243e981c4b5d4e3f4e
Jarne Deschacht <jarne.deschacht@student.hogent.be>
2019-05-16 14:06:33 +0200

    fixed bugs with controlling Activities

--- Taijitan/src/domain/ActivityType.java
+++ Taijitan/src/domain/ActivityType.java
@@ -2,7 +2,24 @@ package domain;
 
 public enum ActivityType {
 
-    excursie,
-    stage,
+    Excursie(0),
+    Stage(1);
+
+    private int id;
+
+    ActivityType(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static ActivityType getById(int id) {
+        for (ActivityType c : values()) {
+            if (c.id == id) return c;
+        }
+        return null;
+    }
 
 }
